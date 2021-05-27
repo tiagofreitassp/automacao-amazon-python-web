417a8f2eb7ce302af38c558253be9ee88017d465
Tiago Freitas <tiago.freitas.sp@gmail.com>
2021-05-27 14:50:06 -0300

    Inserido funcao ChromeDriverManager na classe environment

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def before_all(context):
-	context.web = webdriver.Chrome('./drivers/chromedriver')
+	#context.web = webdriver.Chrome('./drivers/chromedriver')
+	context.web = webdriver.Chrome(ChromeDriverManager().install())
 	context.web.maximize_window()
 	context.web.implicitly_wait(15)
 
